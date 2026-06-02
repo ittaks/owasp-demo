335de3bd2c597417b5d145a25d81436e8cc26489
Marija <mivkovic@mikos.io>
2026-06-02 14:58:52 +0200

    update for a5

--- src/main/java/com/demo/owasp/dto/request/TaskRequest.java
+++ src/main/java/com/demo/owasp/dto/request/TaskRequest.java
@@ -11,13 +11,19 @@ import lombok.Data;
 @Data
 public class TaskRequest {
 
+
     @NotBlank(message = "Naslov ne smije biti prazan.")
     @Size(min = 3, max = 100, message = "Naslov mora imati između 3 i 100 znakova.")
-    // Sprječavamo unos HTML tagova i specijalnih skriptnih znakova (XSS i HTML Injection obrana)
     @Pattern(regexp = "^[a-zA-Z0-9 IdŠšĐđČčĆćŽž.,!?-]*$", message = "Naslov sadrži nedopuštene znakove.")
     private String title;
 
     @NotBlank(message = "Opis ne smije biti prazan.")
     @Size(min = 10, max = 2000, message = "Opis može imati najviše 2000 znakova.")
+    // UKLANJANJE PROPUSTA (XSS): Primjena restriktivnog regularnog izraza koji dopušta tekst, brojeve i osnovnu interpunkciju,
+    // ali eksplicitno zabranjuje znakove <, >, & i ostale elemente koji se koriste za HTML/Script injekciju.
+    @Pattern(
+            regexp = "^[a-zA-Z0-9 IdŠšĐđČčĆćŽž.,!?()\"'#:\n\r-]*$",
+            message = "Opis sadrži nedopuštene znakove (HTML i skriptne oznake nisu dopuštene)."
+    )
     private String description;
 }
