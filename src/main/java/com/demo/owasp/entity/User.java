003cec4f85ea145d2cc7b04d68cc995fef4ef85a
Marija <mivkovic@mikos.io>
2026-06-17 15:28:31 +0200

    actions zap master

--- src/main/java/com/demo/owasp/entity/User.java
+++ src/main/java/com/demo/owasp/entity/User.java
@@ -3,10 +3,12 @@ package com.demo.owasp.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "users")
 public class User {
 
     @Id
